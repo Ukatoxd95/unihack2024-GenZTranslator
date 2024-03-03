438aaa6fdb679f4a0b655265beeb2787f4c6759c
jcho0140 <jchoi0395@gmail.com>
2024-03-03 11:32:56 +1100

    Removed Webpage Translation Button
    Removing webpage translation feature due to restricted access for chrome browser making it difficult to implement during hackathon

--- popup.js
+++ popup.js
@@ -1,33 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var translateToGenZ = true; // Default direction
 
-let translateToGenZ = true; // Default direction
-
-chrome.storage.local.get("selectedText", (data) => {
-    const selectedText = data.selectedText;
-    if (selectedText) {
-        document.getElementById("englishText").value = selectedText;
-    }
-});
-
-document.getElementById('switchDirectionBtn').addEventListener('click', function() {
-    let btn = document.getElementById('switchDirectionBtn');
-    translateToGenZ = !translateToGenZ; // Correctly toggles the translation direction
-    let isArrowRight = btn.innerHTML === "&#8594;" || btn.innerHTML === "→";
-    btn.innerHTML = isArrowRight ? "&#8592;" : "&#8594;";
-});
-
-document.getElementById('translateBtn').addEventListener('click', function() {
-    // Determine which text area to use based on the direction of translation
-    var sourceTextId = translateToGenZ ? 'englishText' : 'genzText';
-    var sourceText = document.getElementById(sourceTextId).value;
-    translateText(sourceText, translateToGenZ);
-});
-
-document.getElementById('translateWebPageBtn').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "translateWebPage"});
+    chrome.storage.local.get("selectedText", (data) => {
+        const selectedText = data.selectedText;
+        if (selectedText) {
+            document.getElementById("englishText").value = selectedText;
+        }
     });
-});
+
+    document.getElementById('switchDirectionBtn').addEventListener('click', function() {
+        var btn = document.getElementById('switchDirectionBtn');
+        translateToGenZ = !translateToGenZ; // Correctly toggles the translation direction
+        console.log(translateToGenZ ? "Left to Right" : "Right to Left");
+        var isArrowRight = btn.innerHTML === "&#8594;" || btn.innerHTML === "→";
+        btn.innerHTML = isArrowRight ? "&#8592;" : "&#8594;";
+    });
+
+    document.getElementById('translateBtn').addEventListener('click', function() {
+        // Determine which text area to use based on the direction of translation
+        var sourceTextId = translateToGenZ ? 'englishText' : 'genzText';
+        var sourceText = document.getElementById(sourceTextId).value;
+        console.log("Translating: " + sourceText)
+        translateText(sourceText, translateToGenZ);
+    });
+
 
 });
 
@@ -70,21 +66,20 @@ function translateWebPage() {
 }
 
 function translateEnglishToGenz(text, json) {
-    //
+    // Go through each key and check if it can replace any sub strings
     var newString = text.toLowerCase();
     for (let key in json) {
         if (newString.includes(key)){
             newString = newString.replaceAll(key, json[key])
-            console.log(key)
         }   
     }
-
     return newString
 }
 
 function translateGenzToEnglish(text, json) {
+    
     var newString = text.toLowerCase();
-
+    // Checks if the key exists in the json file and replaces gen z slang with a random word
     newString = newString.split(' ');
     for (let i = 0; i < newString.length; i++) {
         if (newString[i] in json) {
